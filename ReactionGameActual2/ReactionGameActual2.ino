175566d7ba06f2c7be1a2654410b130714bd4d23
411Group1 <klgibbs@klgibbs-mac01.local>
2014-12-12 12:04:31 -0800

    final code upload.
    changed the way the button press is captured

--- ReactionGameActual2/ReactionGameActual2.ino
+++ ReactionGameActual2/ReactionGameActual2.ino
@@ -1,3 +1,5 @@
+//arduino compiler has no brain!
+
 #include "SPI.h"
 #include "Adafruit_GFX.h"
 #include "Adafruit_ILI9340.h"
@@ -22,16 +24,35 @@ const int SwitchThree = 2;
 const int SwitchFour = A2;
 const int SwitchReset = A0;
 
+const int MAX_LONG = 2147483647;
+
 boolean playerOneJoined = false;
 boolean playerTwoJoined = false;
 boolean playerThreeJoined = false;
 boolean playerFourJoined = false;
 
+// This is how many milliseconds a button must be done to be a press
+const int ButtonMinPress = 25;
+
+// Button state is the last state of the button
+// Button count is how many times through the loop the button is the same value
+// Button pressed is true when button is stable (low) for N counts then is released (high)
+// Button pressed gets cleared by game logic after it checks the value
 int buttonState1 = 1;
+int buttonCount1 = 0;
 int buttonState2 = 1;
+int buttonCount2 = 0;
 int buttonState3 = 1;
+int buttonCount3 = 0;
 int buttonState4 = 1;
+int buttonCount4 = 0;
 int buttonStateR = 1;
+int buttonCountR = 0;
+
+boolean buttonPressed1 = false;
+boolean buttonPressed2 = false;
+boolean buttonPressed3 = false;
+boolean buttonPressed4 = false;
 
 int winner = 0;
 
@@ -48,18 +69,105 @@ typedef enum {
   
 } GameState;  
 
-GameState gGameState;
+int gGameState;
 
-//Number of ms passed since start of game (+ = 10)
-long gameTime = 0;
 long beginTime = 0;
 long startGameTime = 0;
 long choosePlayersTime = 0;
 long playAgainTimer = 0;
 
+// Check the four player buttons for down and up
+void scanButtons() {
+	int b1;
+	int b2;
+	int b3;
+	int b4;
+	
+	b1 = digitalRead(SwitchOne);
+	b2 = digitalRead(SwitchTwo);
+	b3 = digitalRead(SwitchThree);
+	b4 = digitalRead(SwitchFour);
+	
+	// Check if button 1 changed state
+	if (b1 != buttonState1) {
+		// If button was released after a long enough press, report it
+		if (b1 == HIGH && buttonCount1 >= ButtonMinPress) {
+			buttonPressed1 = true;
+		}
+		// Reset button count because it changed state
+		buttonCount1 = 0;
+	}
+	else {
+		// The button state didn't change this time, increment counter
+		if (buttonCount1 < ButtonMinPress) buttonCount1++;
+	}
+	
+	// Check if button 2 changed state
+	if (b2 != buttonState2) {
+		// If button was released after a long enough press, report it
+		if (b2 == HIGH && buttonCount2 >= ButtonMinPress) {
+			buttonPressed2 = true;
+		}
+		// Reset button count because it changed state
+		buttonCount2 = 0;
+	}
+	else {
+		// The button state didn't change this time, increment counter
+		if (buttonCount2 < ButtonMinPress) buttonCount2++;
+	}
+	
+	// Check if button 3 changed state
+	if (b3 != buttonState3) {
+		// If button was released after a long enough press, report it
+		if (b3 == HIGH && buttonCount3 >= ButtonMinPress) {
+			buttonPressed3 = true;
+		}
+		// Reset button count because it changed state
+		buttonCount3 = 0;
+	}
+	else {
+		// The button state didn't change this time, increment counter
+		if (buttonCount3 < ButtonMinPress) buttonCount3++;
+	}
+
+	// Check if button 4 changed state
+	if (b4 != buttonState4) {
+		// If button was released after a long enough press, report it
+		if (b4 == HIGH && buttonCount4 >= ButtonMinPress) {
+			buttonPressed4 = true;
+		}
+		// Reset button count because it changed state
+		buttonCount4 = 0;
+	}
+	else {
+		// The button state didn't change this time, increment counter
+		if (buttonCount4 < ButtonMinPress) buttonCount4++;
+	}
+	
+	// Remember the state of the button for next time
+	buttonState1 = b1;
+	buttonState2 = b2;
+	buttonState3 = b3;
+	buttonState4 = b4;
+}
+
+void advanceGameState(int nextState){
+
+	// Update global
+	gGameState = nextState;
+	// Clear any pending buttons
+	buttonPressed1 = false;
+	buttonPressed2 = false;
+	buttonPressed3 = false;
+	buttonPressed4 = false;
+}
+
 void resetGame(){
-gGameState = GameState_Startup;
-gameTime = 0;
+  playerOneJoined = false;
+  playerTwoJoined = false;
+  playerThreeJoined = false;
+  playerFourJoined = false;
+    advanceGameState(GameState_Startup);
 }
 
 
@@ -82,13 +190,15 @@ void loop(){
   resetGame();
   boolean done = false;
   
-  long playerOneTime = 0;
-  long playerTwoTime = 0;
-  long playerThreeTime = 0;
-  long playerFourTime = 0;
+  long playerOneTime = MAX_LONG;
+  long playerTwoTime = MAX_LONG;
+  long playerThreeTime = MAX_LONG;
+  long playerFourTime = MAX_LONG;
   
   do {
-    
+	// Check buttons first thing every time through the loop
+    scanButtons();
+	
     switch (gGameState) {
       
       case GameState_Startup:
@@ -96,26 +206,25 @@ void loop(){
         delay(2000);
         rules();
         delay(5000);
-        playerOneTime = 0;
-        playerTwoTime = 0;
-        playerThreeTime = 0;
-        playerFourTime = 0;
+        playerOneTime = MAX_LONG;
+        playerTwoTime = MAX_LONG;
+        playerThreeTime = MAX_LONG;
+        playerFourTime = MAX_LONG;
         play();
-        gGameState = GameState_WaitForPlayButton;
+        startGameTime = millis();
+        advanceGameState(GameState_WaitForPlayButton);
         break;
         
       case GameState_WaitForPlayButton:
         buttonStateR = digitalRead(SwitchReset);
         if (buttonStateR == HIGH){   
-            buttonState1 = digitalRead(SwitchOne);
-            if (buttonState1 == LOW){
-              choosePlayersTime = millis();
+            if (buttonPressed1){
               howManyPlayers();
-              gGameState = GameState_WaitForPlayersToJoin;
+              choosePlayersTime = millis();
+              advanceGameState(GameState_WaitForPlayersToJoin);
             }
-            startGameTime = millis();
-              if (millis() > startGameTime + 10000){
-              resetGame(); 
+            if (millis() > startGameTime + 10000){
+                resetGame(); 
             }
         }
         else{
@@ -126,28 +235,26 @@ void loop(){
       case GameState_WaitForPlayersToJoin:
         buttonStateR = digitalRead(SwitchReset);
         if (buttonStateR == HIGH){
-          buttonState1 = digitalRead(SwitchOne);
-          if (buttonState1 == LOW){
+          if (buttonPressed1){
             playerOneJoined = true;
           }
-          buttonState2 = digitalRead(SwitchTwo);
-          if(buttonState2 == LOW){
+          if(buttonPressed2){
             playerOneJoined = true;
             playerTwoJoined = true;
           }
-          buttonState3 = digitalRead(SwitchThree);
-          if(buttonState3 == LOW){
+          if(buttonPressed3){
             playerOneJoined = true;
             playerTwoJoined = true;
             playerThreeJoined = true;
           }
-          buttonState4 = digitalRead(SwitchFour);
-          if (buttonState4 == LOW){  
+          if (buttonPressed4){  
             playerOneJoined = true;
             playerTwoJoined = true;
+            playerThreeJoined = true;
             playerFourJoined = true;
            }
-          if (millis() > choosePlayersTime + 3000) {
+          if (millis() > choosePlayersTime + 5000) {
+            // Maybe we should just 
             if(playerOneJoined == false && playerTwoJoined == false && playerThreeJoined == false && playerFourJoined == false){
               tft.fillScreen(ILI9340_BLACK);
               tft.setCursor(50,50);
@@ -156,11 +263,11 @@ void loop(){
               tft.println("");
               tft.println("Reset Game");
               delay(1000);
-             gGameState = GameState_Startup; 
+			  advanceGameState(GameState_Startup);
             }    
             else{
               // Done adding players, show how many joined on display
-              gGameState = GameState_DisplayNumOfPlayers;
+              advanceGameState(GameState_DisplayNumOfPlayers);
             }
           }
         }
@@ -175,7 +282,7 @@ void loop(){
           //display number of players
           displayNumberOfPlayers();
           delay(2000);
-          gGameState = GameState_DisplayRandomGo;
+          advanceGameState(GameState_DisplayRandomGo);
         }
         else{
          resetGame(); 
@@ -189,7 +296,7 @@ void loop(){
           go();
           beginTime = millis();
           Serial.println(beginTime);
-          gGameState = GameState_WaitForPlayerInputOrTimeout;
+          advanceGameState(GameState_WaitForPlayerInputOrTimeout);
         }
         else{
          resetGame(); 
@@ -199,38 +306,34 @@ void loop(){
       case GameState_WaitForPlayerInputOrTimeout:
         buttonStateR = digitalRead(SwitchReset);
         if (buttonStateR == HIGH){
-          buttonState1 = digitalRead(SwitchOne);
-          if (buttonState1 == LOW){
+          if (buttonPressed1){
           // record when player press his button
-            if(playerOneTime == 0){
+            if(playerOneTime == MAX_LONG){
               playerOneTime = millis() - beginTime;
             }
           }
-          buttonState2 = digitalRead(SwitchTwo);
-          if (buttonState2 == LOW){
+          if (buttonPressed2){
           // record when player press his button
-            if(playerTwoTime == 0){
+            if(playerTwoTime == MAX_LONG){
             playerTwoTime = millis() - beginTime;
             }
           }
-          buttonState3 = digitalRead(SwitchThree);
-          if (buttonState3 == LOW){
+          if (buttonPressed3){
           // record when player press his button
-            if(playerThreeTime == 0){
+            if(playerThreeTime == MAX_LONG){
             playerThreeTime = millis() - beginTime;
             }
           }
-          buttonState4 = digitalRead(SwitchFour);
-          if (buttonState4 == LOW){
+          if (buttonPressed4){
           // record when player press his button
-            if(playerFourTime == 0){
+            if(playerFourTime == MAX_LONG){
             playerFourTime = millis() - beginTime;
             }
           }
           if (millis() > beginTime + 3000) {
             roundCompleted();
             delay(2000);
-            gGameState = GameState_EvaluatePlayerScores;
+            advanceGameState(GameState_EvaluatePlayerScores);
           }
         }
          else{
@@ -245,39 +348,39 @@ void loop(){
             winner = 1;
           } 
           if(playerOneJoined == true && playerTwoJoined == true && playerThreeJoined == false && playerFourJoined == false){
-            if(playerOneTime != 0 && playerOneTime <= playerTwoTime){
+            if(playerOneTime != MAX_LONG && playerOneTime <= playerTwoTime){
               winner = 1; 
             }
-            if(playerTwoTime != 0 && playerTwoTime < playerOneTime){
+            if(playerTwoTime != MAX_LONG && playerTwoTime < playerOneTime){
               winner = 2; 
              }
           }
           if(playerOneJoined == true && playerTwoJoined == true && playerThreeJoined == true && playerFourJoined == false){
-             if(playerOneTime != 0 && playerOneTime <= playerTwoTime && playerOneTime <= playerThreeTime){
+             if(playerOneTime != MAX_LONG && playerOneTime <= playerTwoTime && playerOneTime <= playerThreeTime){
                winner = 1; 
              }
-            if(playerTwoTime != 0 && playerTwoTime < playerOneTime && playerTwoTime <= playerThreeTime){
+            if(playerTwoTime != MAX_LONG && playerTwoTime < playerOneTime && playerTwoTime <= playerThreeTime){
               winner = 2; 
             }
-            if(playerThreeTime != 0 && playerThreeTime < playerOneTime && playerThreeTime < playerTwoTime){
+            if(playerThreeTime != MAX_LONG && playerThreeTime < playerOneTime && playerThreeTime < playerTwoTime){
               winner = 3; 
             }
           }
           if(playerOneJoined == true && playerTwoJoined == true && playerThreeJoined == true && playerFourJoined == true){
-            if(playerOneTime != 0 && playerOneTime <= playerTwoTime && playerOneTime <= playerThreeTime && playerOneTime <= playerFourTime){
+            if(playerOneTime != MAX_LONG && playerOneTime <= playerTwoTime && playerOneTime <= playerThreeTime && playerOneTime <= playerFourTime){
               winner = 1; 
             }
-            if(playerTwoTime != 0 && playerTwoTime < playerOneTime && playerTwoTime <= playerThreeTime && playerTwoTime <= playerFourTime){
+            if(playerTwoTime != MAX_LONG && playerTwoTime < playerOneTime && playerTwoTime <= playerThreeTime && playerTwoTime <= playerFourTime){
               winner = 2; 
             }
-            if(playerThreeTime != 0 && playerThreeTime < playerOneTime && playerThreeTime < playerTwoTime && playerThreeTime <= playerFourTime){
+            if(playerThreeTime != MAX_LONG && playerThreeTime < playerOneTime && playerThreeTime < playerTwoTime && playerThreeTime <= playerFourTime){
               winner = 3; 
             }
-            if(playerFourTime != 0 && playerFourTime < playerOneTime && playerFourTime < playerTwoTime && playerFourTime < playerThreeTime){
+            if(playerFourTime != MAX_LONG && playerFourTime < playerOneTime && playerFourTime < playerTwoTime && playerFourTime < playerThreeTime){
               winner = 4; 
             }
           }
-        gGameState = GameState_DisplayWinner;
+        advanceGameState(GameState_DisplayWinner);
         }
         else{
          resetGame(); 
@@ -324,10 +427,11 @@ void loop(){
             tft.println("");
             tft.println("   milliseconds"); 
           }
+          winner = 0;
           delay(3000);
           playAgain();
           playAgainTimer = millis();
-          gGameState = GameState_WaitForPlayAgain;
+          advanceGameState(GameState_WaitForPlayAgain);
         }
         else{
          resetGame(); 
@@ -337,14 +441,17 @@ void loop(){
       case GameState_WaitForPlayAgain:
         buttonStateR = digitalRead(SwitchReset);
         if (buttonStateR == HIGH){
-          if(millis() > playAgainTimer + 3000){
-          buttonState1 = digitalRead(SwitchOne);
-          if (buttonState1 == LOW){
-            // Restart the game, with the same number of players
-            gGameState = GameState_DisplayNumOfPlayers;
-            gameTime = 0;
+          if(millis() > playAgainTimer + 2500){
+            if (buttonPressed1){
+              // Restart the game, with the same number of players
+              advanceGameState(GameState_DisplayNumOfPlayers);
+              playerOneTime = MAX_LONG;
+              playerTwoTime = MAX_LONG;
+              playerThreeTime = MAX_LONG;
+              playerFourTime = MAX_LONG;
+            }
           }
-          else{
+          if (millis() > playAgainTimer + 5000) {
             tft.fillScreen(ILI9340_BLACK);
             tft.setCursor(50,50);
             tft.setRotation(3);
@@ -353,14 +460,14 @@ void loop(){
             tft.println("Game Over");
             delay(2000);
             done = true;
-          } 
-         }
+          }
         }
         else{
          resetGame(); 
         }
         break;
     }
+    // throttle button and game logic  (mostly needed for button timing)
+    delay(1);
   } while (!done);  
 }
-
